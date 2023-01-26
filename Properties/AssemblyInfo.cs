9f158435609040da8323e9fb413a59cf59b771c4
Romain Ferraton <rferraton@users.noreply.github.com>
2023-01-26 23:48:02 +0100

    Add Assembly info

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// Les informations générales relatives à un assembly dépendent du
+// jeu d'attributs suivant. Modifiez ces valeurs d'attribut pour modifier les informations
+// associées avec l'assembly SQLCLR.
+[assembly: AssemblyTitle("CLRDateConverters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Architecture & Performance")]
+[assembly: AssemblyProduct("CLRDateConverter")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Les informations de version pour un assembly incluent les quatre valeurs suivantes :
+//
+//      Version majeure
+//      Version mineure 
+//     Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
